74c116511f8dbcd5043e48d08c438a9c8190001b
Chris Margonis <chris.margonis@withplum.com>
2021-03-13 22:41:00 +0200

    Add card

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/widgets/Card.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.widgets
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun RowCard(@DrawableRes image: Int, title: String) {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        backgroundColor = MaterialTheme.colors.surface,
+        elevation = 0.dp,
+        modifier = Modifier
+            .height(56.dp)
+            .width(192.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            Image(
+                modifier = Modifier.size(56.dp),
+                painter = painterResource(id = image),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Card picture"
+            )
+            Text(
+                text = title,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(start = 16.dp, end = 16.dp),
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.onSurface
+            )
+        }
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun Preview() {
+    MyTheme {
+        RowCard(image = R.drawable.quick_yoga, title = "Short Mantras")
+    }
+}
