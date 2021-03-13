8507fdc0d35e84ecd35a546321cf76bdbb9186ed
Chris Margonis <chris.margonis@withplum.com>
2021-03-13 22:36:57 +0200

    Remove unused colors

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,10 +17,6 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
 val white = Color(0xFFFFFFFF)
 val whiteOp80 = Color(0xCCFFFFFF)
 val whiteOp85 = Color(0xD9FFFFFF)
